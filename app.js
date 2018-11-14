1d788fe98e1e47843a94f2cca24ba67460d804a0
Oleg Seriaga <oleg@targetprocess.com>
2018-11-14 12:19:37 +0300

    implement retry to handle 202 and rate limits

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ const JSONStream = require('JSONStream');
 const moment = require('moment');
 const morgan = require('morgan');
 const retry = require('bluebird-retry');
-
+const RETRY_OPTS = {
+    backoff: 5,
+    interval: 1000,
+    max_tries: 3,
+    timeout: 50000
+};
 const getErrorCode = (err) => err.code || err.statusCode || err.status || 500;
 const getMessage = (err) => err.message || err.statusText;
 
@@ -33,17 +38,9 @@ const writeContributorStatsToStream = (entry, repo, contributorName, stream) => 
     });
 };
 
-const writeRepoStatsToStream = async (github, repo, stream, getContributorName) => {
-    const stats = await github.getRepo(repo.owner.login, repo.name).getContributorStats().then(processGitHubResponse);
-    await Promise.each(stats, async (entry) => {
-        const login = entry.author.login;
-        const contributorName = login ? await getContributorName(login) : 'Unknown Member';
-        writeContributorStatsToStream(entry, repo, contributorName, stream);
-    });
-};
-
 const processGitHubResponse = (r) => {
-    if (r.status >= 400) {
+    console.log(`${r.config.method} ${r.status} ${r.config.url} | ${r.statusText}`);
+    if (r.status !== 200) {
         const error = new Error(r.statusText);
         error.code = r.status;
         throw error;
@@ -51,14 +48,35 @@ const processGitHubResponse = (r) => {
     return r.data;
 };
 
+const getRepositories = (github) => retry(() => {
+    return github.getUser().listRepos({'affiliation': 'organization_member'}).then(processGitHubResponse);
+}, RETRY_OPTS);
+
+const getUserProfile = (github, login) => retry(() => {
+    return github.getUser(login).getProfile().then(processGitHubResponse);
+}, RETRY_OPTS);
+
+const getRepoStats = (github, repo) => retry(() => {
+    return github.getRepo(repo.owner.login, repo.name).getContributorStats().then(processGitHubResponse);
+}, RETRY_OPTS);
+
+const writeRepoStatsToStream = async (github, repo, stream, getContributorName) => {
+    const stats = await getRepoStats(github, repo);
+    await Promise.each(stats, async (entry) => {
+        const login = entry.author.login;
+        const contributorName = login ? await getContributorName(login) : 'Unknown Member';
+        writeContributorStatsToStream(entry, repo, contributorName, stream);
+    });
+};
+
 const writeStatsToStream = async (repos, github, stream) => {
     const contributorProfiles = {};
     const getContributorName = async (login) => {
         if (!contributorProfiles[login]) {
-            const profile = await github.getUser(login).getProfile().then(processGitHubResponse) || {name: login};
+            const profile = await getUserProfile(github, login);
             contributorProfiles[login] = profile || {};
         }
-        return contributorProfiles[login].name || login;
+        return contributorProfiles[login].name || login || 'Uknown User';
     };
     await Promise.each(repos, (repo) => writeRepoStatsToStream(github, repo, stream, getContributorName))
         .catch(err => stream.write({
@@ -76,16 +94,16 @@ const getAuthToken = (req) => {
     }
     return req.query.token;
 };
-
 app.use(body.json());
+
 app.use(morgan(':method :status :response-time ms'));
 
 app.get('/', async (req, res, next) => {
     try {
         const accessToken = getAuthToken(req);
         const github = new GitHub({token: accessToken});
-        const me = github.getUser();
-        const repos = await me.listRepos({'affiliation': 'organization_member'}).then(processGitHubResponse);
+        const repos = await getRepositories(github);
+        console.log(`Count of repos to process is ${repos.length}`);
         res.type('json');
         const stream = JSONStream.stringify();
         stream.pipe(res);
