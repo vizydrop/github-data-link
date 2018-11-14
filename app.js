36a7daf60525831b3c3f791dd1f69387eaaaa5be
Oleg Seriaga <oleg@targetprocess.com>
2018-11-14 11:48:52 +0300

    append simple test on content

--- app.js
+++ app.js
@@ -1,12 +1,105 @@
 const express = require('express');
 const app = express();
 const body = require('body-parser');
+const GitHub = require('github-api');
+const Promise = require('bluebird');
+const JSONStream = require('JSONStream');
+const moment = require('moment');
+const morgan = require('morgan');
+const retry = require('bluebird-retry');
+
+const getErrorCode = (err) => err.code || err.statusCode || err.status || 500;
+const getMessage = (err) => err.message || err.statusText;
+
+const writeContributorStatsToStream = (entry, repo, contributorName, stream) => {
+    entry.weeks.forEach(week => {
+        if (week.a + week.d + week.c === 0) {
+            return;
+        }
+        const v = {
+            'Organization': repo.owner.login,
+            'Repository': repo.name,
+            'Repository Created On': moment(repo.created_at).format('DD-MMM-YYYY'),
+            'Repository Language': repo.language || 'N/A',
+            'Repository Size': repo.size,
+            'Is Private Repository': repo.private,
+            'Contributor': contributorName,
+            'Date': moment.unix(week.w).format('DD-MMM-YYYY'),
+            'Code Additions': week.a,
+            'Code Deletions': week.d,
+            'Code Commits': week.c
+        };
+        stream.write(v);
+    });
+};
+
+const writeRepoStatsToStream = async (github, repo, stream, getContributorName) => {
+    const stats = await github.getRepo(repo.owner.login, repo.name).getContributorStats().then(processGitHubResponse);
+    await Promise.each(stats, async (entry) => {
+        const login = entry.author.login;
+        const contributorName = login ? await getContributorName(login) : 'Unknown Member';
+        writeContributorStatsToStream(entry, repo, contributorName, stream);
+    });
+};
+
+const processGitHubResponse = (r) => {
+    if (r.status >= 400) {
+        const error = new Error(r.statusText);
+        error.code = r.status;
+        throw error;
+    }
+    return r.data;
+};
+
+const writeStatsToStream = async (repos, github, stream) => {
+    const contributorProfiles = {};
+    const getContributorName = async (login) => {
+        if (!contributorProfiles[login]) {
+            const profile = await github.getUser(login).getProfile().then(processGitHubResponse) || {name: login};
+            contributorProfiles[login] = profile || {};
+        }
+        return contributorProfiles[login].name || login;
+    };
+    await Promise.each(repos, (repo) => writeRepoStatsToStream(github, repo, stream, getContributorName))
+        .catch(err => stream.write({
+            __streamError: {
+                message: getMessage(err),
+                code: getErrorCode(err)
+            }
+        }));
+};
+
+const getAuthToken = (req) => {
+    const accessToken = (req.headers['authorization'] || '').replace('token ', '');
+    if (accessToken) {
+        return accessToken;
+    }
+    return req.query.token;
+};
 
 app.use(body.json());
+app.use(morgan(':method :status :response-time ms'));
 
-app.get('/', (req, res) => {
-    res.json([]);
+app.get('/', async (req, res, next) => {
+    try {
+        const accessToken = getAuthToken(req);
+        const github = new GitHub({token: accessToken});
+        const me = github.getUser();
+        const repos = await me.listRepos({'affiliation': 'organization_member'}).then(processGitHubResponse);
+        res.type('json');
+        const stream = JSONStream.stringify();
+        stream.pipe(res);
+        await writeStatsToStream(repos, github, stream);
+        stream.end();
+    } catch (err) {
+        next(err.response || err);
+    }
 });
+
+app.use((err, req, res, next) => res.status(getErrorCode(err)).send({
+    message: getErrorCode(err),
+    code: getErrorCode(err)
+}));
 
 const server = app.listen(process.env.PORT || 8080);
 module.exports = () => server;
--- app.test.suite.js
+++ app.test.suite.js
@@ -2,7 +2,8 @@ const chai = require('chai');
 const expect = chai.expect;
 const request = require('supertest');
 
-describe('github data link suite', () => {
+describe('github data link suite', function () {
+    this.timeout(100000);
     before('start app', () => {
         this.app = require('./app')();
         this.get = (config = {}) => request(`http://127.0.0.1:${this.app.address().port}`)
@@ -14,9 +15,28 @@ describe('github data link suite', () => {
         expect(process.env.token).to.be.a('string');
     });
 
-    it('should not be authenticated to github with non-valid token', () => this.get({ token: 'not-valid'}).expect(401));
+    it('should not be authenticated to github with non-valid token', () => this.get({token: 'not-valid'}).expect(401));
 
-    it('should be authenticated to github with provided token', () => this.get().expect(200));
+    it('should be retrieve stats with provided token', () => this.get()
+        .expect(200)
+        .then((res) => {
+            const stats = res.body;
+            expect(stats).to.be.a('array');
+            expect(stats).to.have.length.above(0);
+            const entry = stats[0];
+            expect(entry).to.have.property('Code Additions');
+            expect(entry).to.have.property('Code Deletions');
+            expect(entry).to.have.property('Code Commits');
+            expect(entry).to.have.property('Code Commits');
+            expect(entry).to.have.property('Organization');
+            expect(entry).to.have.property('Repository');
+            expect(entry).to.have.property('Repository Created On');
+            expect(entry).to.have.property('Repository Language');
+            expect(entry).to.have.property('Repository Size');
+            expect(entry).to.have.property('Is Private Repository');
+            expect(entry).to.have.property('Contributor');
+            expect(entry).to.have.property('Date');
+        }));
 
     after((done) => this.app.close(done));
 });
